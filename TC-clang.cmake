05c471c98ca78e5adcc92bf8471d5c7c6a3f5921
hastmark <hastmark2001@hotmail.com>
2024-11-24 18:00:37 +0100

    Added stricted build flags

--- TC-clang.cmake
+++ TC-clang.cmake
@@ -5,4 +5,4 @@ set(CMAKE_CXX_COMPILER clang++)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
 
-set(CMAKE_CXX_FLAGS_DEBUG_INIT "-Wpedantic -Wextra -Werror")
+set(CMAKE_CXX_FLAGS_DEBUG_INIT "-std=c++20 -Wpedantic -Wall -Wextra -Werror -Wshadow -Wdeprecated -Wconversion")
